43d3d4b989873226032d8cb7ab23dab28c23d1a6
Rey <reynaldigazali@gmail.com>
2026-06-27 02:00:31 +0800

    seventh

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
                 },
             },
             script: {
-                propsDestructure: false,
+                definePropsLoose: true,
             },
         }),
         wayfinder({
